9627241843882ff63c1d3324c361d100c5dabd49
nirajanmandal <officialnirajan@gmail.com>
2023-12-19 21:41:48 +0545

    refactor: implement zustand and complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import Container from "./components/layout/Container";
 import Footer from "./components/layout/Footer";
 import HashtagList from "./components/HashtagList";
-import FeedbackItemsContextProvider from "./components/contexts/FeedbackItemsContextProvider";
+import { useFeedbackItemsStore } from "./stores/feedbackItemsStore";
+import { useEffect } from "react";
 
 function App() {
+  const fetchFeedbackItems = useFeedbackItemsStore(
+    (state) => state.fetchFeedbackItems
+  );
+
+  useEffect(() => {
+    fetchFeedbackItems();
+  }, [fetchFeedbackItems]);
+
   return (
     <div className="app">
       <Footer />
-      <FeedbackItemsContextProvider>
-        <Container />
-        <HashtagList />
-      </FeedbackItemsContextProvider>
+      <Container />
+      <HashtagList />
     </div>
   );
 }
--- src/components/HashtagList.tsx
+++ src/components/HashtagList.tsx
@@ -1,15 +1,17 @@
-import { useFeedbackItemsContext } from "../lib/hooks";
+import { useFeedbackItemsStore } from "../stores/feedbackItemsStore";
 import HashTagItem from "./hashtag/HashTagItem";
 
 export default function HashtagList() {
-  const { companyList, handleSelectCompany } = useFeedbackItemsContext();
+
+  const companyList = useFeedbackItemsStore((state) => state.getCompanyList());
+  const selectCompany = useFeedbackItemsStore((state) => state.selectCompany);
   return (
     <ul className="hashtags">
       {companyList.map((company) => (
         <HashTagItem
           key={company}
           company={company}
-          onSelectCompany={handleSelectCompany}
+          onSelectCompany={selectCompany}
         />
       ))}
     </ul>
